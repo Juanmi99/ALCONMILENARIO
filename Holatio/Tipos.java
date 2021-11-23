7d93b5bf5c6eaf2a5cdcb7ee4c00529bda3deec7
Juanmi Dakxter <Juanmi Dakxter@Dakxter-PC>
2021-11-23 09:13:00 +0100

    enviando

---
+++ Holatio/Tipos.java
@@ -0,0 +1,42 @@
+package Holatio;
+
+public class Tipos {
+
+	public static void main(String[] args) {
+		System.out.println(" Tipos");
+		// Comentarid de 1 línea
+		/*
+		 * Comentario de bloque
+		 */
+		
+		//Tipos de Variable
+		//Tipo Entero
+		byte b; // (8 bits)
+		short sh; // (16 bits)
+		int i; // (32 bits)
+		long l; // (64 bits)
+		
+		char c; 
+		char a = 'a';
+		System.out.println((int)a);
+		
+		// Tipo Real
+		float f = 3.146F; //32 bits
+		double d = 3.1416; //64 bits+
+		
+		// Tipo lógico
+		boolean bol = true;
+		
+		//Cadenas
+		String cadena = "Holaque tal ";
+		String cadena2 = "estas";
+		System.out.println(cadena + cadena2);
+		
+		
+		//Tipos complejos
+		Integer ii = 23; 
+		System.out.println(ii.MAX_VALUE);
+		
+		System.out.println();
+	}
+}
